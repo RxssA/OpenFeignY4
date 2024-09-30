097d4e9ed604931dbd1bf8ffc811cd42790e2ab1
G00401655@atu.ie <G00401655@atu.ie>
2024-09-30 16:37:50 +0100

    First Push

---
+++ src/main/java/ie/atu/openfeignrecapw3/OpenFeignRecapW32Application.java
@@ -0,0 +1,13 @@
+package ie.atu.openfeignrecapw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeignRecapW32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignRecapW32Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/openfeignrecapw3/OpenFeignRecapW32ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.openfeignrecapw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFeignRecapW32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
